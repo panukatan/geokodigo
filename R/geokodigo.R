f820e3a7b658e96a4d37ef5ce5e2ce6cc0026bee
Ernest Guevarra <ernest@guevarra.io>
2024-03-31 10:00:08 +0100

    initial commit

---
+++ R/geokodigo.R
@@ -0,0 +1,21 @@
+#'
+#' An Interface to the Philippine Standard Geographical Codes
+#'
+#' The Philippine Standard Geographic Code (PSGC) is a classification and coding
+#' of the geographical-political subdivisions of the country, such as the
+#' region, the province, the municipality/city and the barangay. The PSGC is
+#' currently compiled and updated quarterly through a collaborative effort of
+#' the Philippine Statistics Authority (PSA) and its interagency Technical
+#' Working Group on Geographic Code (TWGGC) composed of the Department of the
+#' Interior and Local Government (DILG), Commission on Elections (COMELEC),
+#' National Mapping and Resource Information Authority (NAMRIA), and the Land
+#' Management Bureau (LMB). This package provides utilities for accessing
+#' different versions of the PSGC over time, formatting and structuring them
+#' into machine-readable relational database-ready formats.
+#'
+#' @name geokodiog
+#' @docType package
+#' @keywords internal
+#'
+"_PACKAGE"
+
